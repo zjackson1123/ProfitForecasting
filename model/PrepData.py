fixed denormalizing function, mean and standard deviation are still wildly incorrect due to floating point inaccuracy

model/PrepData.py
@@ -19,28 +19,28 @@ class PrepData():
         test_len = round(d_len)
         data_list = [data[:train_len], data[train_len:val_len], data[val_len:test_len]]
         global undo_n
-        undo_n = {}
+        undo_n = []
         for i in data_list:
             for x in i.columns:
                 std = np.std(i[x].values)
-                mean = np.mean(i[x].values)
+                mean = np.mean(i[x].values, dtype=np.float64)
                 values = [(y-mean)/std for y in i[x].values]
-                i[x] = values
-                undo_n[x] = [std, mean]
+                i[x].iloc[:] = values
+            undo_n = np.append(undo_n, np.array([std, mean]))
+        undo_n = undo_n.reshape(len(data_list), 2)
 
         return data_list[0], data_list[1], data_list[2]
 
-    def denormalize(self, data_list):
+    def denormalize(self, data):      
         
-        #(100,2)
-        for i in data_list:
-            for x in i.columns:
-                std = undo_n[x][0]
-                mean = undo_n[x][1]
-                values = [(y*std) + mean for y in i[x].values]
-                i[x] = values
-        return data_list[0], data_list[1], data_list[2]
-    
+        std = undo_n[-1,0] 
+        mean = undo_n[-1, 1]
+        v_arr = data.numpy()
+        for i in range(v_arr.shape[1]):
+            values = [(y*std) + mean for y in v_arr[i]]
+            v_arr[i] = values
+        return data[0], data[1], data[2]
+           
     def prep_data(self, data):
        train_data, val_data, test_data = self.normalize(data)     
        return train_data, val_data, test_data

model/WindowGen.py
@@ -2,7 +2,7 @@ from pickle import TRUE
 import numpy as np
 import matplotlib.pyplot as plot
 import tensorflow as tf
-import Model.PrepData as pd
+import model.PrepData as pd
 from sklearn import metrics
 from io import BytesIO
 import base64
@@ -156,8 +156,8 @@ class temp():
 
     FeedBack.call = call
 
-    def compile_and_fit(model, window, patience=15):
-        MAX_EPOCHS = 300
+    def compile_and_fit(model, window, patience=5):
+        MAX_EPOCHS = 30
         stop = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=patience, mode='min', restore_best_weights=True)
         model.compile(loss=tf.keras.losses.MeanSquaredError(),
                 optimizer=tf.keras.optimizers.Adam(),
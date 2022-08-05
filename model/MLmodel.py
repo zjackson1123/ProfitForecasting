found egregious data normalization error and fixed it, continuing to experiment with different activations/weight constraints/dropouts for accuracy

model/MLmodel.py
@@ -14,14 +14,17 @@ def Predict():
     dataframe = pandas.read_csv(path, header=None, names=names, parse_dates=[0], dtype=numpy.float64, index_col=0, skiprows=1)
     dataframe = dataframe.iloc[:,[0]].astype(numpy.float64)
     window = win.temp.WindowGen(input_width=100, label_width=25, shift=25, data=dataframe)
-    feedback_model = win.temp.FeedBack(units=32, out_steps=25)
+    feedback_model = win.temp.FeedBack(units=16, out_steps=25)
     history, batch_size = win.temp.compile_and_fit(feedback_model, window)
     plot_url = window.plot(feedback_model)
-    loss = np.min(history.history['loss'])
-    rmse = np.min(history.history['root_mean_squared_error'])
+    index = history.history['val_loss'].index(np.min(history.history['val_loss']))
+    t_loss = history.history['loss'][index]
+    t_rmse = history.history['root_mean_squared_error'][index]
+    v_rmse = history.history['val_root_mean_squared_error'][index]
+    v_loss = history.history['val_loss'][index]
     epoch_num = len(history.epoch)
 
-    return plot_url, loss, rmse, epoch_num, batch_size
+    return plot_url, t_loss, t_rmse, v_loss, v_rmse, epoch_num, batch_size
 
 
 

model/PrepData.py
@@ -6,46 +6,47 @@ import math
 
 
 class PrepData():
-    def scale(self, data):
-        global scaler
-        scaler = MinMaxScaler(feature_range=(0,1))
-        values = scaler.fit_transform(data.values)
-        data = pandas.DataFrame(values,columns=data.columns,index=data.index)
-        return data
-
     def normalize(self, data):
         d_len = len(data)
         train_len = round(d_len*.7)
         val_len = round(d_len*.9)
-        test_len = round(d_len)
-        data_list = [data[:train_len], data[train_len:val_len], data[val_len:test_len]]
-        global undo_n
-        undo_n = []
-        for i in data_list:
-            for x in i.columns:
-                mean = (math.fsum(i[x].values))/len(i[x].values)
-                var = sum(pow(c-mean,2) for c in i[x].values) / len(i[x].values)
-                std = math.sqrt(var)
-                values = [(y-mean)/std for y in i[x].values]
-                i[x].iloc[:] = values
-            undo_n = np.append(undo_n, np.array([std, mean]))
-        undo_n = undo_n.reshape(len(data_list), 2)
-
-        return data_list[0], data_list[1], data_list[2]
+        global std
+        global mean
+        mean = np.mean(data)
+        std = np.std(data)
+        data.iloc[:] = [(x-mean)/std for x in data.values] 
+        train_data = data[:train_len]
+        val_data = data[train_len:val_len]
+        test_data = data[val_len:]
+        return train_data, val_data, test_data
 
     def denormalize(self, data):             
-        std = undo_n[-1,0] 
-        mean = undo_n[-1, 1]
         v_arr = data.numpy()
         for i in range(v_arr.shape[1]):
             values = [(y*std) + mean for y in v_arr[:, i]]
             v_arr[:, i] = values
         return v_arr
+
+    def scale(self, data):
+        global scaler
+        scaler = MinMaxScaler(feature_range=(0,1))
+        d_len = len(data)
+        train_len = round(d_len*.7)
+        val_len = round(d_len*.9)
+        data_list = [data[:train_len], data[train_len:val_len], data[val_len:]]
+        for i in range(len(data_list)):                         
+            data_list[i] = pandas.DataFrame(data=scaler.fit_transform(data_list[i]), columns=data_list[i].columns, index=data_list[i].index)
+        return data_list[0], data_list[1], data_list[2]
+
+    def invert_scale(self, data):
+        return scaler.inverse_transform(data)
            
     def prep_data(self, data):
        train_data, val_data, test_data = self.normalize(data)     
+       #train_data, val_data, test_data = self.scale(data) 
        return train_data, val_data, test_data
 
     def revert_data(self, data):
        data = self.denormalize(data)
+       #data = self.invert_scale(data)
        return data

model/WindowGen.py
@@ -2,6 +2,7 @@ from pickle import TRUE
 import numpy as np
 import matplotlib.pyplot as plot
 import tensorflow as tf
+from tensorflow.keras.constraints import 
 import model.PrepData as pd
 from sklearn import metrics
 from io import BytesIO
@@ -59,7 +60,7 @@ class temp():
             if model is not None:
                 predictions = model(inputs)
                 predictions = revert.revert_data(predictions[n,:,:])
-                plot.plot(self.label_indices, predictions[:, label_col_index], marker='.', label='Prediction', c='#ff7f0e')
+                plot.plot(self.label_indices, predictions[:, label_col_index], marker='.', label='Prediction', c='#ff7f0e', zorder=-10)
             if n == 0:
                 plot.legend()
         img = BytesIO()
@@ -128,10 +129,10 @@ class temp():
             super().__init__()
             self.out_steps = out_steps
             self.units = units
-            self.lstm_cell = tf.keras.layers.LSTMCell(units, activation='tanh')
-            self.lstm_rnn = tf.keras.layers.RNN(self.lstm_cell, return_state=True)
-            self.lstm_cell2 = tf.keras.layers.LSTMCell(units)
-            self.dense = tf.keras.layers.Dense(1, activation='relu')
+            self.lstm_cell = tf.keras.layers.LSTMCell(units, activation='tanh', kernel_contraint=tf.keras.constraints.MaxNorm(3))
+            self.lstm_rnn = tf.keras.layers.RNN(self.lstm_cell, return_state=True, kernel_contraint=tf.keras.constraints.MaxNorm(3))
+            self.dropout = tf.keras.layers.Dropout(0.2)
+            self.dense = tf.keras.layers.Dense(1)
             
 
     def warmup(self, inputs):
@@ -146,8 +147,7 @@ class temp():
       predictions.append(prediction)
       for n in range(1, self.out_steps):
         x = prediction
-        x, state = self.lstm_cell(x, states=state,
-                                  training=training)
+        x, state = self.lstm_cell(x, states=state, training=training)
         prediction = self.dense(x)
         predictions.append(prediction)
       predictions = tf.stack(predictions)
@@ -156,17 +156,18 @@ class temp():
 
     FeedBack.call = call
 
-    def compile_and_fit(model, window, patience=10):
+    def compile_and_fit(model, window, patience=7):
         MAX_EPOCHS = 150
-        stop = tf.keras.callbacks.EarlyStopping(monitor='val-loss', patience=patience, mode='min', restore_best_weights=True)
-        model.compile(loss=tf.keras.losses.MeanSquaredError(),
-                optimizer=tf.keras.optimizers.Adam(),
-                metrics=[tf.keras.metrics.RootMeanSquaredError()])
+        stop = tf.keras.callbacks.EarlyStopping(monitor='loss', patience=patience, mode='min', restore_best_weights=True)
+        optimizer = tf.keras.optimizers.SGD(learning_rate=0.1, momentum=0.9)
+        model.compile(loss=tf.keras.losses.MeanAbsoluteError(),
+                optimizer=optimizer,
+                metrics=['accuracy'])
 
 
         history = model.fit(window.train, epochs=MAX_EPOCHS,
-                      validation_data=window.val)#, 
-                      #callbacks=stop)
+                      validation_data=window.val, 
+                      callbacks=stop, verbose=2)
                       
         return history, batch_size
 